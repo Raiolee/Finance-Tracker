d691a429aa162ef59827610bd1f51537a136278d
Noe Railey Vierneza <114850923+Raiolee@users.noreply.github.com>
2024-10-09 21:55:26 +0800

    Added a incomes table

--- Database/PennyWise.sql
+++ Database/PennyWise.sql
@@ -10,3 +10,15 @@ CREATE TABLE User_Registration_Data
     Email VARCHAR(50),
     Password VARCHAR(255)
 );
+
+CREATE TABLE incomes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    investment VARCHAR(255) NOT NULL,
+    source VARCHAR(255) NOT NULL,
+    total DECIMAL(10, 2) NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    category ENUM('Monthly', 'Weekly', 'Yearly') NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
