3c8e5d106244b226cdf943dc8e91b94db644db34
Noe Railey Vierneza <114850923+Raiolee@users.noreply.github.com>
2024-10-09 21:56:26 +0800

    Made the connection a function to be able to call into other files

--- connection/config.php
+++ connection/config.php
@@ -1,8 +1,17 @@
 <?php
-$DB_Host = "localhost";
-$DB_User = "root";
-$DB_Password = "";
-$DB_Name = "user_db";
+// Database Connection Function
+function getDatabaseConnection() {
+    $DB_Host = "localhost";
+    $DB_User = "root";
+    $DB_Password = "";
+    $DB_Name = "user_db";
 
-$conn = mysqli_connect($DB_Host, $DB_User, $DB_Password, $DB_Name);
+    $conn = mysqli_connect($DB_Host, $DB_User, $DB_Password, $DB_Name);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    return $conn;
+}
 ?>
