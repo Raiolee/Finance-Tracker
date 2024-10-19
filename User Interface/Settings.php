5642a2d475da91cf1643b42665283583ef2a4f93
Noe Railey Vierneza <114850923+Raiolee@users.noreply.github.com>
2024-10-19 20:00:05 +0800

    Modified Settings # added front-end

--- User Interface/Settings.php
+++ User Interface/Settings.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION["user"]))
-{
+if (!isset($_SESSION["user"])) {
     header("Location: ../Login.php");
 }
 $username = $_SESSION["name"];
@@ -9,14 +8,18 @@ $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Styles/Interface1.css">
+    <link rel="stylesheet" href="../Styles/styles.scss">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Styles/Interface1.css">
+    <link href='https://fonts.googleapis.com/css?family=Cabin Condensed' rel='stylesheet'>
     <title>Dashboard</title>
 </head>
+
 <body class="container">
     <div class="nav-bar">
         <div class="Profile">
@@ -76,7 +79,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
             </div>
             <div>
                 <p><a href="Savings.php">Savings</a></p>
-            </div>            
+            </div>
         </div>
 
         <!-- Settings Section -->
@@ -95,13 +98,42 @@ $current_page = basename($_SERVER['PHP_SELF']);
             </div>
         </div>
     </div>
+    <!-- Settings Page -->
+    <section class="main-section">
+        <div class="main-container">
+            <div class="content">
+                <div class="top-bar">
+                    <h1 class="settings">Settings</h1>
+                </div>
+                <!-- Profile Button -->
+                <div class="buttons">
+                    <h2 class="pfp-labels">Profile</h2>
 
-    <div class="content">
-        <div class="right-container">
-            <p>Hello World</p>
+                </div>
+                <!-- Notifications Button -->
+                <div class="buttons">
+                    <h2 class="pfp-labels">Notifications</h2>
+                </div>
+                <!-- Back up and Sync Button -->
+                <div class="buttons">
+                    <h2 class="pfp-labels">Back up and Sync</h2>
+                </div>
+                <!-- Help and Support Button -->
+                <div class="buttons">
+                    <h2 class="pfp-labels">Help and Support</h2>
+                </div>
+
+            </div>
+            <!-- Sign Out Button -->
+            <div class="sign-out">
+                <button class="sign-out-button"><a href=""></a>Sign Out</button>
+            </div>
         </div>
-    </div>
+    </section>
+
+
 
 
 </body>
+
 </html>
