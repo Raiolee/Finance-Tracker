ae2de711d701de512fb96ee5731c485983f1c647
NathanielEDU <nathanielalmeriaedu@gmail.com>
2024-11-03 16:19:27 +0800

    removed login.php session modal allocate

--- User Interface/modals/modal-allocate.php
+++ User Interface/modals/modal-allocate.php
@@ -3,12 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
-if (!isset($_SESSION["user"])) {
-    header("Location: ../Login.php");
-    exit();
-}
-
 $uid = $_SESSION["user_id"];
 $username = $_SESSION["name"];
 $current_page = basename($_SERVER['PHP_SELF']);
