f154d6b818f15269f44d4227187e8995329a4571
Noe Railey Vierneza <114850923+Raiolee@users.noreply.github.com>
2024-11-05 18:44:07 +0800

    Refactor search form and mobile navbar styles; enhance responsiveness and improve table structure

--- User Interface/navbar-mobile.php
+++ User Interface/navbar-mobile.php
@@ -1,10 +1,19 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+<link rel="stylesheet" href="../Styles/mobilestyles.scss">
 <!-- Top Navigation Menu -->
 <div class="navbar-mobile">
-    <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
-    <a href="Dashboard.php" class="active">Logo</a>
+    <!-- Hamburger menu icon (left) -->
+    <a href="javascript:void(0);" class="icon-left" onclick="navbarFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
 
+    <!-- Centered Logo -->
+    <a href="Dashboard.php" class="center-icon">PennyWise</a>
+
+    <!-- Search icon (right) -->
+    <a href="javascript:void(0);" class="icon-right">
+        <i class="fa fa-search"></i>
+    </a>
 
     <!-- Navigation links (hidden by default) -->
     <div class="navbarLinks" id="navbarLinks">
@@ -15,10 +24,6 @@
         <a href="Savings.php" class="navbarItems">Banks</a>
         <a href="Settings.php" class="navbarItems">Settings</a>
     </div>
-    <a href="javascript:void(0);" class="icon" onclick="navbarFunction()">
-        <i class="fa fa-bars"></i>
-    </a>
 </div>
-
 
 <script src="../js/navbar.js"></script>
